4dadd6ff3c221e6e75261afb135cfef658150fb3
Glenn Lehman <admin@glennlehman.com>
2024-10-02 14:42:52 -0400

    Switched from UnitTest to pytest, revised file structure, updated README, added .gitignore, CONTRIBUTORS.md, and requirements.txt

--- models/pyramid.py
+++
@@ -1,5 +0,0 @@
-class Pyramid:
-    def calculate_blocks(self, height):
-        if height < 1:
-            raise ValueError("Height must be at least 1.")
-        return sum(i**2 for i in range(1, height+1))
